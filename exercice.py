698fbc5f3981ad3829c6f79172a2bca4413892f8
EbonGit <leben06@outlook.fr>
2022-11-16 14:31:46 -0500

    v0

--- exercice.py
+++ exercice.py
@@ -1,8 +1,23 @@
 from game import *
 
+def deal_damage(a,b):
+	print(f"{b.name} used {b.weapon.name}")
+	print(f'  {a.name} took {b.compute_damage(a)} dmg')
+
+def run_battle(a,b):
+	return None
+
 
 def main():
-	pass
+	c1 = Character("Äpik", 200, 150, 70, 70)
+	c2 = Character("Gämmor", 250, 100, 120, 60)
+
+	c1.weapon = Weapon("BFG", 100, 69)
+	c2.weapon = Weapon("Deku Stick", 120, 1)
+
+	deal_damage(c1, c2)
+
+
 
 
 if __name__ == "__main__":
--- game.py
+++ game.py
@@ -11,6 +11,20 @@ import utils
 
 
 class Weapon:
+	def __init__(self,name,power,min_level):
+		self.__name = name
+		self.power = power
+		self.min_level = min_level
+
+	@property
+	def name(self):
+		return self.__name
+
+	UNARMED_POWER = 20
+
+	@classmethod
+	def make_unarmed(cls):
+		return cls("Unarmed", cls.UNARMED_POWER, 1)
 	"""
 	Une arme dans le jeu.
 
@@ -19,10 +33,32 @@ class Weapon:
 	:param min_level: Le niveau minimal pour l'utiliser
 	"""
 
-	UNARMED_POWER = 20
 
 
 class Character:
+
+	def __init__(self, name, max_hp, attack, defense, level):
+		self.__name = name
+		self.max_hp = max_hp
+		self.attack = attack
+		self.defense = defense
+		self.level = level
+		self.weapon = None
+		self.hp = max_hp
+
+	@property
+	def name(self):
+	    return self.__name
+
+	def compute_damage(self,other):
+		proba = random.randint(0,15)
+		crit = 1
+		if proba == 15:
+			crit = 2
+		rand = random.uniform(0.85,1)
+
+		return (((((2*self.level)/5)+2) * self.weapon.power * (self.attack/other.defense)) / 50 + 2) * crit * rand
+
 	"""
 	Un personnage dans le jeu
 
@@ -42,3 +78,4 @@ def deal_damage(attacker, defender):
 def run_battle(c1, c2):
 	# TODO: Initialiser attaquant/défendeur, tour, etc.
 	pass
+
